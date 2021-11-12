3d162cd3d784c7b693ee20d1cceaa4e65093aba6
XDcat-x7p <514269236@qq.com>
2021-11-12 21:34:29 +0800

    加权图

--- v2/output_procon_analysis.py
+++ v2/output_procon_analysis.py
@@ -12,7 +12,9 @@ import os
 from scipy.special import comb
 from itertools import combinations
 from collections import defaultdict
-
+from brokenaxes import brokenaxes
+from matplotlib.gridspec import GridSpec
+from sklearn import preprocessing
 # 日志
 import logging
 import logconfig
@@ -50,19 +52,19 @@ class AnalysisMutationGroup:
 class ProConNetwork:
     def __init__(self,
                  data_dir="../data/procon",
-                 analysis="../data/procon/analysis.json",
                  parse1="../data/procon/type1_parse.csv",
                  parse2="../data/procon/type2_parse.csv",
                  fasta_file="../data/YP_009724390.1.txt",
-                 procon_threshold=300,
                  ):
         self.data_dir = data_dir
-        self.procon_threhold = procon_threshold  # 确定网络阈值
 
         log.info("构造图...")
         # procon 计算的所有的结果
         self.type1 = pd.read_csv(parse1)  # 单点
         self.type2 = pd.read_csv(parse2)  # 成对
+        # 归一化: 单点
+        self.type1["info_norm"] = self._normalize_info(self.type1["information"].values)
+        self.type2["info_norm"] = self._normalize_info(self.type2["info"].values)
 
         # fasta 序列
         self.fasta = next(SeqIO.parse(fasta_file, "fasta")).seq
@@ -74,9 +76,15 @@ class ProConNetwork:
         # 构成图
         self.G = self._get_G(self.links, self.nodes)
 
-        log.info("确定中心性...")
         # 中心性
+        log.info("确定中心性...")
         self.degree_c, self.betweenness_c, self.closeness_c = self._get_centralities()
+
+    @staticmethod
+    def _normalize_info(info: np.ndarray):
+        info = info.reshape(-1, 1)
+        mms = preprocessing.MinMaxScaler().fit(info)
+        return mms.transform(info)
 
     @staticmethod
     def _aa2position(aa: str):
@@ -91,18 +99,14 @@ class ProConNetwork:
         nodes = ["{}{}".format(i + 1, j) for i, j in enumerate(fasta)]
         nodes = pd.DataFrame({"position": nodes})
         nodes = pd.merge(nodes, type1, how="left", left_on="position", right_on="position")
-        nodes = nodes.loc[:, ["position", "information"]]
+        nodes = nodes.loc[:, ["position", "info_norm"]]
         nodes.columns = ["name", "size"]
         nodes = nodes.fillna(0)
-        log.debug("nodes = %s", nodes)
         return nodes
 
     def _get_links(self, type2, ):
-        type2 = type2[type2["info"] > self.procon_threhold]  # TODO: 选择阈值
-        log.debug("type2 = %s", type2)
-        links = type2.loc[:, ["site1", "site2", "info"]]
+        links = type2.loc[:, ["site1", "site2", "info_norm"]]
         links.columns = ["source", "target", "weight"]
-        log.debug("links = %s", links)
         return links
 
     def _get_G(self, links, nodes):
@@ -116,8 +120,13 @@ class ProConNetwork:
         return G
 
     def display(self):
+        log.debug("self.type1 = %s", self.type1)
+        log.debug("self.type2 = %s", self.type2)
         log.info("nodes = %s", self.nodes)
         log.info("links = %s", self.links)
+        log.debug("dc = %s", self.degree_c)
+        log.debug("cc = %s", self.closeness_c)
+        log.debug("bc = %s", self.betweenness_c)
 
     @staticmethod
     def count_distribution(d: dict):
@@ -129,14 +138,38 @@ class ProConNetwork:
 
     def _get_centralities(self):
         # 点度中心性 degree
-        dc = degree_centrality(self.G)
-        # 中介中心性 betweenness
-        bc = betweenness_centrality(self.G)
-        # 接近中心性 closeness
-        cc = closeness_centrality(self.G)
-        # log.debug("dc = %s", dc)
-        # log.debug("cc = %s", cc)
-        # log.debug("bc = %s", bc)
+        outpath = [os.path.join(self.data_dir, "cache", f"{i}_centrality.json") for i in ["degree", "betweenness", "closeness"]]
+        self.centrality_cache_dir = outpath
+        if all([os.path.exists(i) for i in outpath]):
+            log.debug("中心性存在缓存，直接读取")
+            log.info("点度中心性 degree")
+            with open(outpath[0], ) as f:
+                dc = json.load(f)
+            # 中介中心性 betweenness
+            log.info("中介中心性 betweenness")
+            with open(outpath[1], ) as f:
+                bc = json.load(f)
+            # 接近中心性 closeness
+            log.info("接近中心性 closeness")
+            with open(outpath[2], ) as f:
+                cc = json.load(f)
+
+        else:
+            log.debug("中心性无缓存，直接计算")
+            log.info("点度中心性 degree")
+            dc = degree_centrality(self.G)
+            with open(outpath[0], "w") as f:
+                f.write(json.dumps(dc))
+            # 中介中心性 betweenness
+            log.info("中介中心性 betweenness")
+            bc = betweenness_centrality(self.G, weight="weight")
+            with open(outpath[1], "w") as f:
+                f.write(json.dumps(bc))
+            # 接近中心性 closeness
+            log.info("接近中心性 closeness")
+            cc = closeness_centrality(self.G, distance="weight")
+            with open(outpath[2], "w") as f:
+                f.write(json.dumps(cc))
         return dc, bc, cc
 
     def analysis_aa(self, aa: str):
@@ -162,7 +195,20 @@ class ProConNetwork:
         fig: plt.Figure
         axs: List[plt.Axes]
         fig, axs = plt.subplots(2, 1)
-        fig.suptitle("threshold={}".format(self.procon_threhold))
+        # sps1, sps2 = GridSpec(2, 1)
+        # fig = plt.figure()
+        # hspace 双斜线间隔
+        # bax = brokenaxes(ylims=((0, 0.02), (0.8, 0.9)), hspace=0.2, subplot_spec=sps1, fig=fig, height_ratios=(3, 8))
+        # bax_axes_botton:plt.Axes = bax.axs[1]
+        # bax_axes_botton.set_yticklabels()
+        # t = bax_axes_botton.get_yticks()
+        # bax_axes_botton.set_yticks(np.arange(0, 0.01, 10))
+        # bax_axes_botton.set_yticklabels(np.arange(0, 0.01, 10))
+        # log.debug("bax_axes_top.get_ymajorticklabels= %s", bax_axes_botton.get_ymajorticklabels())
+
+        # bax_axes_top.get_ymajorticklabels
+        # bax = brokenaxes(ylims=((0, 0.02), (0.8, 0.9)), hspace=0.2, subplot_spec=sps1, fig=fig, )
+        # axs = [bax, plt.subplot(sps2)]
         axs[0].plot(x[1:], y[1:])
         axs[0].set_ylabel("degree distribution")
         # axs[0].plot(x, y)
@@ -189,12 +235,13 @@ class ProConNetwork:
         ax_1_right.bar(count_degree_aa.keys(), count_degree_aa.values(), color="green")
 
         fig.show()
-        fig.savefig(os.path.join(self.data_dir, f"度分布-{self.procon_threhold}.png"), dpi=300)
+        fig.savefig(os.path.join(self.data_dir, f"度分布.png"), dpi=300)
 
     def _plot_node_box(self, aas):
         nodes_size = {node: self.G.nodes[node]["size"] for node in self.G.nodes}
-        node_data = pd.DataFrame({"conservation": nodes_size, "degree centrality": self.degree_c, "closeness centrality": self.closeness_c,
-                                  "betweenness centrality": self.betweenness_c})
+        node_data = pd.DataFrame(
+            {"conservation": nodes_size, "degree centrality": self.degree_c, "closeness centrality": self.closeness_c,
+             "betweenness centrality": self.betweenness_c})
         # node_data = node_data.reset_index()  # 调整索引
         node_data["is_mutation"] = node_data.index.map(lambda x: x in aas)
 
@@ -218,20 +265,14 @@ class ProConNetwork:
 if __name__ == '__main__':
     start_time = time.time()
     # 保守性网络
-    pcn = ProConNetwork(procon_threshold=300)
+    pcn = ProConNetwork()
     # 需要关注的变异
     groups = AnalysisMutationGroup()
     aas = groups.get_non_duplicated_aas()
     log.debug("aas = %s", aas)
 
     pcn.analysisG(aas)
-    # for aa in aas:
-    #     res = pcn.analysis_aa(aa)
-    #     log.debug("%s %s", aa, res)
-    #
-    # for group in groups.get_aa_groups():
-    #     res = pcn.analysis_group(group)
-    #     log.debug("%s\t%s", group, res)
+
 
     end_time = time.time()
     log.info(f"程序运行时间: {end_time - start_time}")
